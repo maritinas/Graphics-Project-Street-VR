da16b26e5273d0d28a5a441edd8cd2933fa38944
sanaa-syed268 <50550249+sanaa-syed268@users.noreply.github.com>
2024-05-16 18:37:44 +0200

    BIKE HAS FALLEN!

--- Assets/Scripts/RotateAndTranslateObject.cs
+++ Assets/Scripts/RotateAndTranslateObject.cs
@@ -1,48 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+
 
 
 public class RotateAndTranslateObject : MonoBehaviour
 {
-    public GameObject bikeToMove;
+    public Transform bikeToMove;
+    public Transform bikeOgPos;
     public Transform targetPosition;
+    private int count = 0;
     public float moveSpeed;  // Speed of movement
 
     // Update is called once per frame
     public void SlideBike()
     {
-        Vector3 targetDirection = targetPosition.position - bikeToMove.transform.position;
-        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-        Debug.Log("Bike is rotating");
 
-        // Only consider rotation around the X axis
-        Quaternion xRotation = Quaternion.Euler(targetRotation.eulerAngles.x, 0, 0);
-
-        bikeToMove.transform.rotation = xRotation;
-
-        bikeToMove.transform.position = Vector3.MoveTowards(
-            bikeToMove.transform.position,
-            targetPosition.position,
-            moveSpeed * Time.deltaTime);
-
-        // the second argument, upwards, defaults to Vector3.up
-       /* Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-        bikeToMove.transform.rotation = rotation;*/
-
-        //set the up direction
-        /* bikeToMove.transform.rotation = Quaternion.RotateTowards(
-             bikeToMove.transform.rotation,
-             targetRotation,
-             moveSpeed * Time.deltaTime);*/
-        bikeToMove.transform.position = Vector3.MoveTowards(
-       bikeToMove.transform.position,
-       targetPosition.position,
-       moveSpeed * Time.deltaTime);
-
+        Debug.Log("Slidebike activated");
+        // Check if the reference is assigned
+        if (bikeToMove != null)
+        {
+            Debug.Log("BIKE detected");
+            if(count==0)
+            {
+                // Rotate the specific object by 90 degrees on the Z-axis
+                bikeToMove.Rotate(90, 0, 0);
+                Vector3 translation = targetPosition.position - bikeToMove.position;
+                // Move the bike to the target position
+                bikeToMove.Translate(translation, Space.World);
+                Debug.Log("turn around only once");
+                count = count + 1;
+            }
+            
+        }
     }
 
 }

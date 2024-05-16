e652b1ffd72e94b663e68bef63827dcce6264ffc
Konstantina Maria Sourmpati <kmsou@kth.se>
2024-05-16 15:34:33 +0200

    Tree Interaction

---
+++ Assets/GazeInteraction/Scripts/LeavesFalling.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeavesFalling : MonoBehaviour
+{
+    // Reference to the GameObject you want to activate
+    public GameObject particleObject;
+
+    // Method to set the GameObject as active
+    public void LeavesSetActive()
+    {
+        if (particleObject != null)
+        {
+            particleObject.SetActive(true);    
+        }
+    }
+
+    public void LeavesDeactivate()
+    {
+        if (particleObject != null)
+        {
+            particleObject.SetActive(false);
+        }
+    }
+
+    
+}
